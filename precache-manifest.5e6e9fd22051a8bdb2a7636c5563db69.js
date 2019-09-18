fbed4ba593bd3c3968a8b3c5e0f8ecae3bcc9146
user <user@MacBook-Pro-user.local>
2019-09-18 19:30:01 +0300

    Updates

---
+++ precache-manifest.5e6e9fd22051a8bdb2a7636c5563db69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4671c7245a2abe8d931b",
+    "url": "./static/js/main.9285377b.chunk.js"
+  },
+  {
+    "revision": "f5ea86449bd048d1abc0",
+    "url": "./static/js/2.38b2a418.chunk.js"
+  },
+  {
+    "revision": "4671c7245a2abe8d931b",
+    "url": "./static/css/main.cadebe36.chunk.css"
+  },
+  {
+    "revision": "f5ea86449bd048d1abc0",
+    "url": "./static/css/2.0c21d4d9.chunk.css"
+  },
+  {
+    "revision": "e32feab4b8192f9550635d27ddd49d33",
+    "url": "./index.html"
+  }
+];
